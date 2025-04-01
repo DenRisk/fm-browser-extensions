469f0f66adef5ba4f658f35cc63d6cf71042a68d
DenRisk <denisniklas.risken@sybit.de>
2025-04-01 16:05:02 +0200

    project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'browser-extensions-manager' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('browser-extensions-manager');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, browser-extensions-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, browser-extensions-manager'
+    );
   });
 });
